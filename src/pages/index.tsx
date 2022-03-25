de348215de00dcedd59829c49dc274085a659cc0
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-03-25 09:52:37 +0100

    Improve landing page links styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,57 @@ import React from 'react';
 
 import Grid from '@mui/material/Grid';
 
-import Link from '../ui/navigation/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import {Link} from 'gatsby';
+
+interface TileProps {
+	to: string;
+}
+
+function Tile({to}: TileProps) {
+	return (
+		<Grid item sm={12} md={6} lg={4} xl={3}>
+			<Button
+				variant="contained"
+				component={Link}
+				to={to}
+				sx={{display: 'block'}}
+			>
+				<Box
+					sx={{
+						minHeight: 200,
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Typography variant="h3">{to}</Typography>
+				</Box>
+			</Button>
+		</Grid>
+	);
+}
+
+interface TilesProps {
+	children: React.ReactNode;
+}
+
+function Tiles({children}: TilesProps) {
+	return (
+		<Grid container spacing={4} padding={1} justifyContent="center">
+			{children}
+		</Grid>
+	);
+}
 
 function IndexPage() {
 	return (
-		<Grid container spacing={2} padding={2}>
-			<Grid item>
-				<Link to="/datainfo">datainfo</Link>
-			</Grid>
-			<Grid item>
-				<Link to="/run_amd">run_amd</Link>
-			</Grid>
-		</Grid>
+		<Tiles>
+			<Tile to="/datainfo" />
+			<Tile to="/run_amd" />
+		</Tiles>
 	);
 }
 
