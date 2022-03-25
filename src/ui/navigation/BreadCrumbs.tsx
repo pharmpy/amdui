a5081d2cd4e5888dc0158009238ca8738aa12981
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-03-25 13:33:27 +0100

    Use Home icon as home link button

--- src/ui/navigation/BreadCrumbs.tsx
+++ src/ui/navigation/BreadCrumbs.tsx
@@ -1,16 +1,29 @@
-import {NavigateNext as NavigateNextIcon} from '@mui/icons-material';
+import {
+	Home as HomeIcon,
+	NavigateNext as NavigateNextIcon,
+} from '@mui/icons-material';
 import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
+import IconButton from '@mui/material/IconButton';
 import React from 'react';
 import Link from './Link';
+
+interface BreadCrumb {
+	basename: React.ReactNode;
+	path: string;
+}
+
+const home: BreadCrumb = {
+	basename: (
+		<IconButton>
+			<HomeIcon />
+		</IconButton>
+	),
+	path: '/',
+};
 
 interface BreadcrumbsProps {
 	path: string;
 }
-
-const home = {
-	basename: 'home',
-	path: '/',
-};
 
 function Breadcrumbs({path}: BreadcrumbsProps) {
 	const parts = path.split('/').filter((part) => part !== '');
