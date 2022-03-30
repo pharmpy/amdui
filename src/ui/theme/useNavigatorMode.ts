2e7973e277ff6e2438677e2665bdb4e3c03f86f8
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-03-30 10:25:19 +0200

    Attempt at fixing first render of UI in dark mode
    Probably SSR not being handled by my implementation. Let's see if
    MUI's implementation solves the problem.

--- src/ui/theme/useNavigatorMode.ts
+++ src/ui/theme/useNavigatorMode.ts
@@ -1,48 +1,6 @@
-import {PaletteMode} from '@mui/material';
-import {useEffect, useState} from 'react';
-import isBrowser from '../../lib/env';
+import {PaletteMode, useMediaQuery} from '@mui/material';
 
-const matchMediaDarkQuery = '(prefers-color-scheme: dark)';
-const matchMediaLightQuery = '(prefers-color-scheme: light)';
-
-const getCurrentNavigatorMode = (): PaletteMode | undefined => {
-	if (isBrowser && window.matchMedia) {
-		if (window.matchMedia(matchMediaDarkQuery).matches) {
-			return 'dark';
-		}
-
-		if (window.matchMedia(matchMediaLightQuery).matches) {
-			return 'light';
-		}
-	}
-
-	return undefined;
-};
-
-const useNavigatorMode = (): PaletteMode | undefined => {
-	const [mode, setMode] = useState<PaletteMode | undefined>(
-		getCurrentNavigatorMode,
-	);
-
-	useEffect(() => {
-		if (window.matchMedia) {
-			const handler = (event: {matches: any}) => {
-				setMode(event.matches ? 'dark' : 'light');
-			};
-
-			const matchDarkMode = window.matchMedia(matchMediaDarkQuery);
-			matchDarkMode.addEventListener('change', handler);
-			if (matchDarkMode.matches) {
-				setMode('dark');
-			}
-
-			return () => {
-				matchDarkMode.removeEventListener('change', handler);
-			};
-		}
-	}, []);
-
-	return mode;
-};
+const useNavigatorMode = (): PaletteMode =>
+	useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light';
 
 export default useNavigatorMode;
